f93b914dc5d9c153f2795f1b355ed28af74ef97f
zlki <137070993+zlki@users.noreply.github.com>
2025-03-05 21:49:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '轩轩',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o2fbu7KTZ6lkLtMn91h4xMkasW7g，',
+      id: 'o2fbu7KTZ6lkLtMn91h4xMkasW7g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'jtPg2nDX8zGgo6jAyvw7TowotXsStlMjQA-PTIr0dLY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
